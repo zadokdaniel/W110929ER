17ead76577d57a708b24bb1086190d9e685f9ad9
Daniel Zadok <zadok.daniel@gmail.com>
2024-12-16 17:55:38 +0200

    ...

---
+++ W110929ER - 2024.12.16/real-app/app.js
@@ -0,0 +1,26 @@
+const PORT = 3005;
+
+const mongoose = require("mongoose");
+const express = require("express");
+
+const usersRouter = require("./routes/users");
+
+const app = express();
+
+app.use(require("morgan")("dev"));
+app.use(express.json());
+
+app.use("/api/users", usersRouter);
+
+connect();
+
+async function connect() {
+  try {
+    await mongoose.connect("mongodb://localhost/myRestAPI_W110929ER");
+
+    console.log("connected to db");
+    app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+  } catch (e) {
+    console.log("failed to connect to db", e.message);
+  }
+}
---
+++ W110929ER - 2024.12.16/real-app/model/users.js
@@ -0,0 +1,41 @@
+const Joi = require("joi");
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 255,
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 6,
+    maxlength: 255,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+    maxlength: 1024,
+  },
+  biz: { type: Boolean, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const User = mongoose.model("User", userSchema, "users");
+
+function validateUser(user) {
+  const schema = Joi.object({
+    name: Joi.string().min(2).max(255).required(),
+    email: Joi.string().min(6).max(255).email().required(),
+    password: Joi.string().min(6).max(1024).required(),
+    biz: Joi.boolean().required(),
+  });
+
+  return schema.validate(user);
+}
+
+module.exports = { User, validateUser };
